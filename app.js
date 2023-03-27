c90e0131364016324b571648c143ed54c6377467
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-03-26 23:14:16 -0400

    Small tweak

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('https://express-intermediate.vercel.app/', indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
